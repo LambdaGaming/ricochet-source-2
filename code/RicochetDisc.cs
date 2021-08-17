2a212a8bd8810ef3c572e23d950c33aef29df9d6
LambdaGaming <starwars18m@gmail.com>
2021-08-17 01:52:46 -0400

    Added more basic functions to player and disc

--- code/RicochetDisc.cs
+++ code/RicochetDisc.cs
@@ -4,21 +4,184 @@ namespace Ricochet
 {
 	class Disc : ModelEntity
 	{
+		public float DiscVelocity { get; set; }
+		public int TotalBounces { get; set; }
+		public RicochetPlayer LockTarget { get; set; }
+		public float NextThink { get; set; }
+		public static readonly int DiscPushMultiplier = 1200;
+
 		public new void Spawn()
 		{
+			if ( !Owner.IsValid() ) return;
 			base.Spawn();
 			SetModel( "models/light_arrow.vmdl" ); // Temporary model until the real one gets ported over
-			Vector3 vel = Owner.EyeRot.Forward * 1000;
-			Position = Owner.Position + Owner.EyeRot.Forward * 100;
+			DiscVelocity = HasPowerup( Powerup.Fast ) ? 1500 : 1000;
+			Vector3 vel = Owner.EyeRot.Forward * DiscVelocity;
+			Position = Owner.Position + ( Owner.EyeRot.Forward * 100 ) + ( Owner.EyeRot.Up * 50 );
 			SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
-			PhysicsGroup.Velocity = vel; // TODO: Make it so the disc isn't affected by gravity and doesn't travel along the z-axis
+			PhysicsGroup.Velocity = vel; // TODO: Make it so the disc doesn't travel along the z-axis
+			PhysicsBody.GravityEnabled = false;
+			TotalBounces = 0;
+			NextThink = 0;
 		}
 		
 		public override void StartTouch( Entity ent )
 		{
 			var ply = ent as RicochetPlayer;
-			if ( ply.IsValid() && ply == Owner ) return;
+			var owner = Owner as RicochetPlayer;
+			if ( ply.IsValid() )
+			{
+				if ( ent == Owner )
+				{
+					if ( ply.OwnerTouchCooldown < Time.Now )
+					{
+						// TODO: Emit disc pickup sound
+						ReturnToThrower();
+					}
+					return;
+				}
+				else if ( ply.EnemyTouchCooldown < Time.Now )
+				{
+					if ( owner.Team != ply.Team )
+					{
+						if ( owner.HasPowerup( Powerup.Freeze ) && !owner.Frozen )
+						{
+							// TODO: Emit freeze sound
+							ply.Freeze();
+							if ( !owner.HasPowerup( Powerup.Hard ) )
+							{
+								owner.EnemyTouchCooldown = Time.Now + 2;
+								return;
+							}
+						}
+
+						if ( owner.HasPowerup( Powerup.Hard ) )
+						{
+							ply.Decapitate( owner );
+							owner.EnemyTouchCooldown = Time.Now + 0.5f;
+						}
+						else
+						{
+							// TODO: Play random hit sound
+							Vector3 direction = ply.Velocity.Normal;
+							ply.Velocity = direction * DiscPushMultiplier;
+
+							if ( !ply.Frozen )
+							{
+								// TODO: Shield flash
+							}
+
+							ply.LastPlayerToHitMe = owner;
+							ply.EnemyTouchCooldown = Time.Now + 2;
+						}
+					}
+				}
+			}
+			else if ( ent is Disc )
+			{
+				if ( ent != this )
+				{
+					// TODO: Emit warp sound and spawn sprite
+					( ent as Disc ).ReturnToThrower();
+					ReturnToThrower();
+				}
+			}
+			else
+			{
+				TotalBounces++;
+				// TODO: Emit hit sound and spark effects
+			}
+		}
+
+		[Event.Tick.Server]
+		protected void Tick()
+		{
+			if ( NextThink > Time.Now ) return;
+			if ( HasPowerup( Powerup.Freeze ) && TotalBounces == 0 )
+			{
+				if ( LockTarget.IsValid() )
+				{
+					// No clue if this works or if its actually needed
+					Vector3 direction = ( LockTarget.Position - Position ).Normal;
+					float dot = DotProduct( Vector3.Forward, direction );
+					if ( dot < 0.6f || ( Owner as RicochetPlayer ).Team == LockTarget.Team )
+					{
+						LockTarget = null;
+					}
+				}
+
+				if ( !LockTarget.IsValid() )
+				{
+					foreach ( RicochetPlayer ply in FindAllByName( "RicochetPlayer" ) )
+					{
+						if ( !ply.IsValid() || ply == Owner ) continue;
+						Vector3 direction = ( ply.Position - Position ).Normal;
+						float dot = DotProduct( Vector3.Forward, direction );
+						if ( dot > 0.6f )
+						{
+							LockTarget = ply;
+							break;
+						}
+					}
+				}
+
+				if ( LockTarget.IsValid() )
+				{
+					Vector3 direction = ( LockTarget.Position - Position ).Normal;
+					Velocity = ( Velocity.Normal + ( direction.Normal * 0.25f ) ).Normal;
+					Velocity *= DiscVelocity;
+					WorldAng = Vector3.VectorAngle( Velocity );
+				}
+			}
+
+			if ( TotalBounces >= 3 || ( HasPowerup( Powerup.Fast ) && TotalBounces >= 1 ) )
+			{
+				if ( TotalBounces > 7 )
+				{
+					ReturnToThrower();
+					return;
+				}
+
+				if ( Owner.IsValid() && ( Owner as RicochetPlayer ).Alive() )
+				{
+					Vector3 direction = ( Owner.Position - Position ).Normal;
+					Velocity = direction * DiscVelocity;
+				}
+				else
+				{
+					Delete();
+				}
+			}
+			NextThink = Time.Now + 0.1f;
+		}
+
+		public bool HasPowerup( Powerup powerup )
+		{
+			var ply = Owner as RicochetPlayer;
+			if ( ply.Powerups.Contains( powerup ) )
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public void ReturnToThrower()
+		{
+			var ply = Owner as RicochetPlayer;
+			if ( HasPowerup( Powerup.Hard ) )
+			{
+				ply.GiveDisc( RicochetPlayer.MaxDiscs );
+			}
+			else
+			{
+				ply.GiveDisc( 1 );
+			}
 			Delete();
+		}
+
+		public static float DotProduct( Vector3 a, Vector3 b )
+		{
+			return a.x * b.x + a.y * b.y + a.z * b.z;
 		}
 	}
 }
--- code/RicochetPlayer.cs
+++ code/RicochetPlayer.cs
@@ -13,10 +13,21 @@ namespace Ricochet
 	partial class RicochetPlayer : Player
 	{
 		public float DiscCooldown { get; set; }
+		public float OwnerTouchCooldown { get; set; }
+		public float EnemyTouchCooldown { get; set; }
+		public float FreezeTimer { get; set; }
+		public int NumDiscs { get; set; }
+		public int Team { get; set; }
+		public bool Frozen { get; set; }
+		public RicochetPlayer LastPlayerToHitMe { get; set; }
 		public List<Powerup> Powerups = new();
+		public static readonly int MaxDiscs = 3;
+		public static readonly int FreezeSpeed = 50;
+		public static readonly int FreezeTime = 7;
 
 		public override void Respawn()
 		{
+			base.Respawn();
 			SetModel( "models/citizen/citizen.vmdl" );
 			Controller = new MinimalWalkController();
 			Animator = new StandardPlayerAnimator();
@@ -27,18 +38,36 @@ namespace Ricochet
 			EnableShadowInFirstPerson = true;
 			Powerups.Clear();
 			DiscCooldown = 0;
-			base.Respawn();
+			OwnerTouchCooldown = 0;
+			EnemyTouchCooldown = 0;
+			FreezeTimer = 0;
+			NumDiscs = MaxDiscs;
+			Team = 0;
+			Frozen = false;
 		}
 		
 		public override void Simulate( Client cl )
 		{
 			base.Simulate( cl );
 			SimulateActiveChild( cl, ActiveChild );
-			if ( IsServer && Input.Pressed( InputButton.Attack1 ) && DiscCooldown <= Time.Now )
+			if ( IsServer && DiscCooldown < Time.Now && NumDiscs > 0 )
 			{
-				LaunchDisc();
-				float cooldown = Powerups.Contains( Powerup.Fast ) ? 0.2f : 0.5f;
-				DiscCooldown = Time.Now + cooldown;
+				if ( Input.Pressed( InputButton.Attack1 ) )
+				{
+					LaunchDisc();
+					float cooldown = Powerups.Contains( Powerup.Fast ) ? 0.2f : 0.5f;
+					DiscCooldown = Time.Now + cooldown;
+					RemoveDisc( 1 );
+				}
+				else if ( Input.Pressed( InputButton.Attack2 ) )
+				{
+					AddPowerup( Powerup.Hard );
+					LaunchDisc();
+					float cooldown = Powerups.Contains( Powerup.Fast ) ? 0.2f : 0.5f;
+					DiscCooldown = Time.Now + cooldown;
+					RemovePowerup( Powerup.Hard );
+					RemoveDisc( MaxDiscs );
+				}
 			}
 		}
 
@@ -74,11 +103,80 @@ namespace Ricochet
 		{
 			return Powerups.Remove( powerup );
 		}
+
+		public bool HasPowerup( Powerup powerup )
+		{
+			if ( Powerups.Contains( powerup ) )
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public void GiveDisc( int num )
+		{
+			NumDiscs = ( int ) MathX.Clamp( NumDiscs += num, 0, MaxDiscs );
+		}
+
+		public void RemoveDisc( int num )
+		{
+			NumDiscs = ( int ) MathX.Clamp( NumDiscs -= num, 0, MaxDiscs );
+		}
+
+		public void Freeze()
+		{
+			// TODO: Glowing render effect
+			RenderColorAndAlpha = new Color32( 0, 0, 200, 230 );
+			( Controller as MinimalWalkController ).WalkSpeed = FreezeSpeed;
+			Frozen = true;
+			FreezeTimer = Time.Now + FreezeTime;
+		}
+
+		public void ClearFreeze()
+		{
+			var walk = Controller as MinimalWalkController;
+			// TODO: Glowing render effect
+			RenderColorAndAlpha = Color32.White;
+			walk.WalkSpeed = walk.DefaultSpeed;
+			Frozen = false;
+		}
+
+		public void Decapitate( RicochetPlayer killer )
+		{
+			if ( Frozen )
+			{
+				Shatter( killer );
+				return;
+			}
+
+			if ( Alive() )
+			{
+				LastPlayerToHitMe = killer;
+				DamageInfo dmg = new() { Damage = 500 };
+				TakeDamage( dmg );
+				// TODO: Emit decap sound, change bodygroup of playermodel, and spawn head model
+			}
+		}
+
+		public void Shatter( RicochetPlayer killer )
+		{
+			if ( Alive() )
+			{
+				LastPlayerToHitMe = killer;
+				DamageInfo dmg = new() { Damage = 500 };
+				TakeDamage( dmg );
+				// TODO: Emit shatter sound
+				RenderAlpha = 1;
+				EnableSolidCollisions = false;
+				// TODO: Spawn head model
+			}
+		}
 	}
 
 	public class MinimalWalkController : WalkController
 	{
 		public new float WalkSpeed = 250.0f;
+		public new float DefaultSpeed = 250.0f;
 		public override void CheckJumpButton() {}
 		public override void StayOnGround() {}
 
